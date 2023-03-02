9a907c8ba41d307bfa82c458c5ee0efc4af4a937
Lualvarad0 <78248845+Lualvarad0@users.noreply.github.com>
2023-03-02 17:32:36 -0500

    Add files via upload

---
+++ Project_01/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int entero = 10;
+        long largo = 1234567890L;
+        double doble = 3.1416;
+        boolean booleano = true;
+        String cadena = "Hola, mundo!";
+        System.out.println("Valor entero: " + entero);
+        System.out.println("Valor largo: " + largo);
+        System.out.println("Valor doble: " + doble);
+        System.out.println("Valor booleano: " + booleano);
+        System.out.println("Valor cadena: " + cadena);
+    }
+}
