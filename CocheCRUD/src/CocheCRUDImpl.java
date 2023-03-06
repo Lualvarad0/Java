14cb05a37ca9fb8407bfb6b461393f861e1c5843
Lualvarad0 <78248845+Lualvarad0@users.noreply.github.com>
2023-03-06 13:02:08 -0500

    Add files via upload

---
+++ CocheCRUD/src/CocheCRUDImpl.java
@@ -0,0 +1,16 @@
+public class CocheCRUDImpl implements CocheCRUD {
+    @Override
+    public void save() {
+        System.out.println("save() method called");
+    }
+
+    @Override
+    public void findAll() {
+        System.out.println("findAll() method called");
+    }
+
+    @Override
+    public void delete() {
+        System.out.println("delete() method called");
+    }
+}
